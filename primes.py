bd638c9e77ca584aa44c2036901908505b14ff2c
vcc64 <39348910+vcc64@users.noreply.github.com>
2023-10-03 20:03:34 +0100

    primes function

--- primes.py
+++ primes.py
@@ -1,6 +1,28 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes < 1:
+        raise ValueError
+
+    lastPrimeNumber = 0
+    
+    for i in range(number_of_primes):
+        currentNumber = lastPrimeNumber
+        currentNumberIsPrime = False
+
+        while (not currentNumberIsPrime):
+            for j in range(currentNumber):
+                if (currentNumber % j == 0):
+                    currentNumber += 1
+                    break
+                if (j == currentNumber-1):
+                    currentNumberIsPrime = True
+                    
+        lastPrimeNumber = currentNumber
+        list[i] = currentNumber
+
     return list
